12104e0bc56fbf27165430b8a62537bb769a5d2d
Luke Chatton <chattonluke@gmail.com>
2017-04-27 09:26:17 -0700

    TGM

--- bukkit/src/main/java/TeamBukkit.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by luke on 4/26/17.
- */
-public class TeamBukkit {
-}
---
+++ bukkit/src/main/java/com/minehut/tgm/TGM.java
@@ -0,0 +1,18 @@
+package com.minehut.tgm;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by luke on 4/27/17.\
+ *
+ * Team Game Manager
+ */
+
+
+public class TGM extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
