6b1675635103b66ddbeb698e5fae77bf70d2c8ee
Luke Chatton <chattonluke@gmail.com>
2017-04-27 10:41:35 -0700

    http client

---
+++ API/src/main/java/com/minehut/teamapi/http/HttpOption.java
@@ -0,0 +1,12 @@
+package com.minehut.teamapi.http;
+
+/**
+ * Flags passed to HttpClient for individual requests.
+ *
+ * TODO nice-to-have: replace HttpClientConfig with this mechanism so
+ * any setting can be overridden per request.
+ */
+public enum HttpOption {
+    NO_RETRY,               // Do not retry the request
+    INFINITE_RETRY          // Retry the request forever
+}
