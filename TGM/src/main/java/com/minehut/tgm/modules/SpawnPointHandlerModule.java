80eae93b9dda7b0a92c2d9440d398b6bf13bdae6
Luke Chatton <chattonluke@gmail.com>
2017-04-30 09:51:42 -0700

    Add Fracture as an example map

--- TGM/src/main/java/com/minehut/tgm/modules/SpawnPointHandlerModule.java
+++ TGM/src/main/java/com/minehut/tgm/modules/SpawnPointHandlerModule.java
@@ -1,12 +1,14 @@
 package com.minehut.tgm.modules;
 
 import com.minehut.tgm.TGM;
+import com.minehut.tgm.join.MatchJoinEvent;
 import com.minehut.tgm.map.SpawnPoint;
 import com.minehut.tgm.match.MatchModule;
 import com.minehut.tgm.match.MatchStatus;
 import com.minehut.tgm.team.MatchTeam;
 import com.minehut.tgm.team.TeamChangeEvent;
 import com.minehut.tgm.user.PlayerContext;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,10 +16,14 @@ import org.bukkit.event.Listener;
 public class SpawnPointHandlerModule extends MatchModule implements Listener {
 
     @EventHandler
+    public void onJoin(MatchJoinEvent event) {
+        event.getPlayerContext().getPlayer().teleport(getTeamSpawn(TGM.getTgm().getTeamManager().getSpectators()).getLocation());
+    }
+
+    @EventHandler
     public void onTeamChange(TeamChangeEvent event) {
         if (TGM.getMatchManager().getMatch().getMatchStatus() == MatchStatus.MID) {
-            SpawnPoint spawnPoint = event.getTeam().getSpawnPoints().get(0);
-            event.getPlayerContext().getPlayer().teleport(spawnPoint.getLocation());
+            event.getPlayerContext().getPlayer().teleport(getTeamSpawn(event.getTeam()).getLocation());
         }
     }
 
@@ -26,10 +32,14 @@ public class SpawnPointHandlerModule extends MatchModule implements Listener {
         for (MatchTeam matchTeam : TGM.getTgm().getTeamManager().getTeams()) {
             if (!matchTeam.isSpectator()) {
                 for (PlayerContext player : matchTeam.getMembers()) {
-                    SpawnPoint spawnPoint = matchTeam.getSpawnPoints().get(0);
-                    player.getPlayer().teleport(spawnPoint.getLocation());
+                    player.getPlayer().teleport(getTeamSpawn(matchTeam).getLocation());
                 }
             }
         }
+    }
+
+    private SpawnPoint getTeamSpawn(MatchTeam matchTeam) {
+        //todo: actually randomize spawn points instead of grabbing first one.
+        return matchTeam.getSpawnPoints().get(0);
     }
 }
