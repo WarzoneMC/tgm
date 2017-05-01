379f0809ab39e204eb068642285ab39608fd17ab
Luke Chatton <chattonluke@gmail.com>
2017-04-30 21:24:39 -0700

    Vanilla scoreboard implementation

--- TGM/src/main/java/com/minehut/tgm/modules/scoreboard/ScoreboardManagerModule.java
+++ TGM/src/main/java/com/minehut/tgm/modules/scoreboard/ScoreboardManagerModule.java
@@ -1,13 +1,18 @@
 package com.minehut.tgm.modules.scoreboard;
 
+import com.minehut.tgm.TGM;
 import com.minehut.tgm.join.MatchJoinEvent;
 import com.minehut.tgm.match.MatchModule;
 import com.minehut.tgm.match.ModuleData;
 import com.minehut.tgm.match.ModuleLoadTime;
+import com.minehut.tgm.modules.team.MatchTeam;
+import com.minehut.tgm.modules.team.TeamManagerModule;
+import com.minehut.tgm.user.PlayerContext;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.scoreboard.Team;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +32,19 @@ public class ScoreboardManagerModule extends MatchModule implements Listener {
     public void onJoin(MatchJoinEvent event) {
         SimpleScoreboard simpleScoreboard = new SimpleScoreboard(ChatColor.AQUA + "Objectives");
         Bukkit.getPluginManager().callEvent(new ScoreboardInitEvent(event.getPlayerContext().getPlayer(), simpleScoreboard));
+
+        for (MatchTeam matchTeam : TGM.get().getModule(TeamManagerModule.class).getTeams()) {
+            Team team = simpleScoreboard.getScoreboard().registerNewTeam(matchTeam.getId());
+            team.setPrefix(matchTeam.getColor().toString());
+            team.setCanSeeFriendlyInvisibles(true);
+            team.setAllowFriendlyFire(false);
+            team.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
+
+            for (PlayerContext player : matchTeam.getMembers()) {
+                team.addEntry(player.getPlayer().getName());
+            }
+        }
+
         simpleScoreboard.send(event.getPlayerContext().getPlayer());
         scoreboards.add(simpleScoreboard);
     }
