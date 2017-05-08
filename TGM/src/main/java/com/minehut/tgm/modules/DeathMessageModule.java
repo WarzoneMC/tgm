9c310711a1bb1e7739011a96f15de3ea89efe661
jorgeberrex <jorgeberrex@hotmail.com>
2017-05-08 00:16:19 -0500

    Add more detailed death messages

--- TGM/src/main/java/com/minehut/tgm/modules/DeathMessageModule.java
+++ TGM/src/main/java/com/minehut/tgm/modules/DeathMessageModule.java
@@ -28,18 +28,64 @@ public class DeathMessageModule extends MatchModule implements Listener {
         if(matchTeam.isSpectator()) return; //stupid spectators
 
         Player killer = null;
-
         String message = "";
+        EntityDamageEvent.DamageCause cause = event.getEntity().getLastDamageCause().getCause();
         if (event instanceof PlayerDeathByPlayerEvent) {
             killer = ((PlayerDeathByPlayerEvent) event).getCause();
             MatchTeam killerTeam = teamManagerModule.getTeam(killer);
-            message = matchTeam.getColor() + event.getPlayer().getName() + ChatColor.GRAY + " was killed by " + killerTeam.getColor() + killer.getName();
+            ItemStack weapon = killer.getInventory().getItemInMainHand();
+            if (cause.equals(EntityDamageEvent.DamageCause.FALL)){
+                if (weapon.getType().equals(Material.BOW))
+                    // Needs a better way to get distance between players
+                    message = matchTeam.getColor() + event.getPlayer().getName() + ChatColor.GRAY + " was shot off a high place by " + killerTeam.getColor() + killer.getName() + ChatColor.GRAY;
+                else
+                    message = matchTeam.getColor() + event.getPlayer().getName() + ChatColor.GRAY + " was thrown off a high place by " + killerTeam.getColor() + killer.getName() + ChatColor.GRAY + " using " + itemToString(weapon);
+            }
+            else if (cause.equals(EntityDamageEvent.DamageCause.VOID)){
+                if (weapon.getType().equals(Material.BOW))
+                    // Needs a better way to get distance between players
+                    message = matchTeam.getColor() + event.getPlayer().getName() + ChatColor.GRAY + " was shot into the void by " + killerTeam.getColor() + killer.getName() + ChatColor.GRAY;
+                else
+                    message = matchTeam.getColor() + event.getPlayer().getName() + ChatColor.GRAY + " was thrown into the void by " + killerTeam.getColor() + killer.getName() + ChatColor.GRAY + " using " + itemToString(weapon);
+            }
+            else if (cause.equals(EntityDamageEvent.DamageCause.PROJECTILE)){
+                int distance = ((Double) killer.getLocation().distance(event.getPlayer().getLocation())).intValue();
+                message = matchTeam.getColor() + event.getPlayer().getName() + ChatColor.GRAY + " was shot by " + killerTeam.getColor() + killer.getName() + ChatColor.GRAY + " from " + distance + " blocks";
+            }
+            else {
+                message = matchTeam.getColor() + event.getPlayer().getName() + ChatColor.GRAY + " was killed by " + killerTeam.getColor() + killer.getName() + ChatColor.GRAY + " using " + itemToString(weapon);
+            }
+
         } else {
-            message = matchTeam.getColor() + event.getPlayer().getName() + ChatColor.GRAY + " died to the environment.";
+            if (cause.equals(EntityDamageEvent.DamageCause.FALL)){
+                message = matchTeam.getColor() + event.getPlayer().getName() + ChatColor.GRAY + " fell from a high place.";
+            }
+            else if (cause.equals(EntityDamageEvent.DamageCause.VOID)){
+                message = matchTeam.getColor() + event.getPlayer().getName() + ChatColor.GRAY + " fell into the void.";
+            }
+            else {
+                message = matchTeam.getColor() + event.getPlayer().getName() + ChatColor.GRAY + " died to the environment.";
+            }
         }
         if (message.length() > 0) {
             broadcastDeathMessage(event.getPlayer(), killer, message);
         }
+
+    }
+    
+    private String itemToString(ItemStack item){
+        if (item == null || item.getType().equals(Material.AIR)){
+            return "their hands";
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        if (item.hasItemMeta() && item.getItemMeta().hasEnchants()) stringBuilder.append("Enchanted ");
+        String materialName = item.getType().toString();
+        for (String word : materialName.split("_")){
+            word = word.toLowerCase();
+            word = word.substring(0, 1).toUpperCase() + word.substring(1) + " ";
+            stringBuilder.append(word);
+        }
+        return stringBuilder.toString().trim();
     }
 
     private void broadcastDeathMessage(Player dead, Player killer, String message) {
