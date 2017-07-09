95f55ef1fb0198db86356a8d67c3abe6b91dac0a
Luke Chatton <chattonluke@gmail.com>
2017-07-09 14:15:33 -0700

    remove mojang auth

--- TGM/src/main/java/com/minehut/tgm/util/itemstack/ItemFactory.java
+++ TGM/src/main/java/com/minehut/tgm/util/itemstack/ItemFactory.java
@@ -1,7 +1,5 @@
 package com.minehut.tgm.util.itemstack;
 
-import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.properties.Property;
 import org.apache.commons.codec.binary.Base64;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -130,26 +128,5 @@ public class ItemFactory {
         meta.setOwner(name);
         skull.setItemMeta(meta);
         return skull;
-    }
-
-    public static ItemStack getSkull(String url) {
-        ItemStack head = new ItemStack(Material.SKULL_ITEM, 1, (short)3);
-        if(url.isEmpty())return head;
-
-
-        SkullMeta headMeta = (SkullMeta) head.getItemMeta();
-        GameProfile profile = new GameProfile(UUID.randomUUID(), null);
-        byte[] encodedData = Base64.encodeBase64(String.format("{textures:{SKIN:{url:\"%s\"}}}", url).getBytes());
-        profile.getProperties().put("textures", new Property("textures", new String(encodedData)));
-        Field profileField;
-        try {
-            profileField = headMeta.getClass().getDeclaredField("profile");
-            profileField.setAccessible(true);
-            profileField.set(headMeta, profile);
-        } catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException e1) {
-            e1.printStackTrace();
-        }
-        head.setItemMeta(headMeta);
-        return head;
     }
 }
