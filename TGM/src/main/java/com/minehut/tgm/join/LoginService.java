68a2b4ade1b310e6a49491a80042b22f71895be1
Draem <owen@gimbel.ca>
2017-07-15 20:07:15 -0400

    Remove unused imports.

--- TGM/src/main/java/com/minehut/tgm/join/LoginService.java
+++ TGM/src/main/java/com/minehut/tgm/join/LoginService.java
@@ -1,8 +1,6 @@
 package com.minehut.tgm.join;
 
-import com.minehut.teamapi.models.UserProfile;
 import com.minehut.tgm.user.PlayerContext;
-import org.bukkit.entity.Player;
 
 /**
  * Created by luke on 4/27/17.
