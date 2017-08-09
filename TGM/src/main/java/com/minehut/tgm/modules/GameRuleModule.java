a980662370934fa75b1982154bfa5438b15a0719
MatrixTunnel <MatrixTunnel@users.noreply.github.com>
2017-08-08 18:55:27 -0700

    Change setting gamerules in main world to onEnable

--- TGM/src/main/java/com/minehut/tgm/modules/GameRuleModule.java
+++ TGM/src/main/java/com/minehut/tgm/modules/GameRuleModule.java
@@ -2,21 +2,16 @@ package com.minehut.tgm.modules;
 
 import com.minehut.tgm.match.Match;
 import com.minehut.tgm.match.MatchModule;
-import org.bukkit.Bukkit;
 import org.bukkit.World;
 
 public class GameRuleModule extends MatchModule {
-
-    static {
-        setGameRules(Bukkit.getWorlds().get(0)); //Set gamerules in main unused world
-    }
 
     @Override
     public void load(Match match) {
         setGameRules(match.getWorld());
     }
 
-    private static void setGameRules(World world) {
+    public static void setGameRules(World world) {
         world.setGameRuleValue("doMobSpawning", "false");
         world.setGameRuleValue("doDaylightCycle", "false");
         world.setGameRuleValue("commandBlockOutput", "false");
