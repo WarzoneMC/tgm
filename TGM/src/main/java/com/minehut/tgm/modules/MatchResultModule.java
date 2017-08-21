1507ce3e05f5b2213a609b36bdc88e2492b508db
_Pear <pearofficialyt@gmail.com>
2017-08-21 18:33:30 -0400

    Removed space before getWinningTeam() and after Congrat message

--- TGM/src/main/java/com/minehut/tgm/modules/MatchResultModule.java
+++ TGM/src/main/java/com/minehut/tgm/modules/MatchResultModule.java
@@ -35,7 +35,6 @@ public class MatchResultModule extends MatchModule implements Listener {
             }
 
             player.sendMessage("" + ChatColor.AQUA + ChatColor.STRIKETHROUGH + "-----------------------------------");
-            player.sendMessage(ChatColor.AQUA + "");
             if (event.getWinningTeam() != null) {
                 player.sendMessage(ChatColor.DARK_PURPLE + "               Winning Team: " + event.getWinningTeam().getColor() + event.getWinningTeam().getAlias());
             } else {
@@ -48,7 +47,6 @@ public class MatchResultModule extends MatchModule implements Listener {
             } else {
                 player.sendMessage(ChatColor.GRAY+ "               Better luck next time!");
             }
-                player.sendMessage(ChatColor.AQUA + "");
                 player.sendMessage("" + ChatColor.AQUA + ChatColor.STRIKETHROUGH + "-----------------------------------");
         }
     }
