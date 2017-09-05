b235e2a85da014c37bf72d3ec450ec6b27417c3b
MatrixTunnel <MatrixTunnel@users.noreply.github.com>
2017-09-05 15:58:57 -0700

    Fix TDM Scoreboard

--- TGM/src/main/java/com/minehut/tgm/modules/tdm/TDMModule.java
+++ TGM/src/main/java/com/minehut/tgm/modules/tdm/TDMModule.java
@@ -1,29 +1,23 @@
 package com.minehut.tgm.modules.tdm;
 
-import com.google.gson.JsonObject;
 import com.minehut.tgm.TGM;
 import com.minehut.tgm.match.Match;
 import com.minehut.tgm.match.MatchModule;
-import com.minehut.tgm.modules.controlpoint.ControlPoint;
 import com.minehut.tgm.modules.points.PointsModule;
 import com.minehut.tgm.modules.scoreboard.ScoreboardInitEvent;
 import com.minehut.tgm.modules.scoreboard.ScoreboardManagerModule;
 import com.minehut.tgm.modules.scoreboard.SimpleScoreboard;
 import com.minehut.tgm.modules.team.MatchTeam;
 import com.minehut.tgm.modules.team.TeamManagerModule;
-import com.minehut.tgm.user.PlayerContext;
 import lombok.Getter;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.logging.Level;
 
 /**
  * Created by Jorge on 9/4/2017.
@@ -54,11 +48,10 @@ public class TDMModule extends MatchModule implements Listener {
         for (MatchTeam matchTeam : teams) {
             if (matchTeam.isSpectator()) continue;
             simpleScoreboard.add(matchTeam.getColor() + getTeamScoreLine(matchTeam), i);
-            teamScoreboardLines.put(matchTeam, i);
+            teamScoreboardLines.put(matchTeam, i++);
             simpleScoreboard.add(matchTeam.getColor() + matchTeam.getAlias(), i++);
             if (teams.indexOf(matchTeam) < teams.size() - 1) {
                 simpleScoreboard.add(matchTeam.getColor() + " ", i++);
-                i++;
             }
         }
     }
@@ -87,7 +80,7 @@ public class TDMModule extends MatchModule implements Listener {
             return;
         }
 
-        Player killer = (Player) event.getEntity().getKiller();
+        Player killer = event.getEntity().getKiller();
         MatchTeam team = teamManager.getTeam(killer);
         incrementPoints(team, 1);
     }
