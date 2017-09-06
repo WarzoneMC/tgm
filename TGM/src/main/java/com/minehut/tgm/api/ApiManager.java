de36b86844e5a8d0aefc031c2a0b48638bbabdbf
MatrixTunnel <MatrixTunnel@users.noreply.github.com>
2017-09-05 23:03:43 -0700

    Redo DeathModule for more accurate kills

--- TGM/src/main/java/com/minehut/tgm/api/ApiManager.java
+++ TGM/src/main/java/com/minehut/tgm/api/ApiManager.java
@@ -9,6 +9,7 @@ import com.minehut.tgm.map.ParsedTeam;
 import com.minehut.tgm.match.MatchLoadEvent;
 import com.minehut.tgm.match.MatchResultEvent;
 import com.minehut.tgm.modules.ChatModule;
+import com.minehut.tgm.modules.DeathModule;
 import com.minehut.tgm.modules.team.MatchTeam;
 import com.minehut.tgm.modules.team.TeamManagerModule;
 import com.minehut.tgm.user.PlayerContext;
@@ -22,57 +23,55 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ApiManager implements Listener {
+
     private ObjectId serverId;
     @Getter private MatchInProgress matchInProgress;
+
+    private DeathModule deathModule;
 
     public ApiManager() {
         this.serverId = new ObjectId();
         TGM.registerEvents(this);
 
-        Bukkit.getScheduler().runTaskTimerAsynchronously(TGM.get(), new Runnable() {
-            @Override
-            public void run() {
-                List<String> players = new ArrayList<>();
-                for (PlayerContext playerContext : TGM.get().getPlayerManager().getPlayers()) {
-                    try {
-                        players.add(playerContext.getUserProfile().getId().toString());
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+        Bukkit.getScheduler().runTaskTimerAsynchronously(TGM.get(), () -> {
+            List<String> players = new ArrayList<>();
+            for (PlayerContext playerContext : TGM.get().getPlayerManager().getPlayers()) {
+                try {
+                    players.add(playerContext.getUserProfile().getId().toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                int maxPlayers = 0;
-                int spectatorCount = 0;
-                int playerCount = 0;
-                for (MatchTeam matchTeam : TGM.get().getModule(TeamManagerModule.class).getTeams()) {
-                    if (matchTeam.isSpectator()) {
-                        spectatorCount += matchTeam.getMembers().size();
-                        continue;
-                    }
-
-                    maxPlayers += matchTeam.getMax();
-                    playerCount += matchTeam.getMembers().size();
-                }
-                Heartbeat heartbeat = new Heartbeat(
-                        TGM.get().getConfig().getString("server.name"),
-                        TGM.get().getConfig().getString("server.id"),
-                        players,
-                        playerCount,
-                        spectatorCount,
-                        maxPlayers,
-                        TGM.get().getMatchManager().getMatch().getMapContainer().getMapInfo().getName(),
-                        TGM.get().getMatchManager().getMatch().getMapContainer().getMapInfo().getGametype().getName(),
-                        serverId
-                );
-                TGM.get().getTeamClient().heartbeat(heartbeat);
             }
+            int maxPlayers = 0;
+            int spectatorCount = 0;
+            int playerCount = 0;
+            for (MatchTeam matchTeam : TGM.get().getModule(TeamManagerModule.class).getTeams()) {
+                if (matchTeam.isSpectator()) {
+                    spectatorCount += matchTeam.getMembers().size();
+                    continue;
+                }
+
+                maxPlayers += matchTeam.getMax();
+                playerCount += matchTeam.getMembers().size();
+            }
+            Heartbeat heartbeat = new Heartbeat(
+                    TGM.get().getConfig().getString("server.name"),
+                    TGM.get().getConfig().getString("server.id"),
+                    players,
+                    playerCount,
+                    spectatorCount,
+                    maxPlayers,
+                    TGM.get().getMatchManager().getMatch().getMapContainer().getMapInfo().getName(),
+                    TGM.get().getMatchManager().getMatch().getMapContainer().getMapInfo().getGametype().getName(),
+                    serverId
+            );
+            TGM.get().getTeamClient().heartbeat(heartbeat);
         }, 20L, 20L);
     }
 
     @EventHandler
     public void onMatchResult(MatchResultEvent event) {
-        if (isStatsDisabled()) {
-            return;
-        }
+        if (isStatsDisabled()) return;
 
         List<String> winners = new ArrayList<>();
         for (PlayerContext playerContext : event.getWinningTeam().getMembers()) {
@@ -89,7 +88,7 @@ public class ApiManager implements Listener {
         TeamManagerModule teamManagerModule = TGM.get().getModule(TeamManagerModule.class);
         List<TeamMapping> teamMappings = new ArrayList<>();
         for (MatchTeam matchTeam : teamManagerModule.getTeams()) {
-            if(matchTeam.isSpectator()) continue;
+            if (matchTeam.isSpectator()) continue;
 
             for (PlayerContext playerContext : matchTeam.getMembers()) {
                 teamMappings.add(new TeamMapping(matchTeam.getId(), playerContext.getUserProfile().getId().toString()));
@@ -111,56 +110,45 @@ public class ApiManager implements Listener {
 
     @EventHandler
     public void onMatchLoad(MatchLoadEvent event) {
-        if (isStatsDisabled()) {
-            return;
-        }
+        if (isStatsDisabled()) return;
+        deathModule = event.getMatch().getModule(DeathModule.class);
 
         MapInfo mapInfo = event.getMatch().getMapContainer().getMapInfo();
         List<com.minehut.teamapi.models.Team> teams = new ArrayList<>();
         for (ParsedTeam parsedTeam : mapInfo.getTeams()) {
             teams.add(new com.minehut.teamapi.models.Team(parsedTeam.getId(), parsedTeam.getAlias(), parsedTeam.getTeamColor().name(), parsedTeam.getMin(), parsedTeam.getMax()));
         }
-        Bukkit.getScheduler().runTaskAsynchronously(TGM.get(), new Runnable() {
-            @Override
-            public void run() {
-                MapLoadResponse mapLoadResponse = TGM.get().getTeamClient().loadmap(new Map(mapInfo.getName(), mapInfo.getVersion(), mapInfo.getAuthors(), mapInfo.getGametype().toString(), teams));
-                Bukkit.getLogger().info("Received load map response. Id: " + mapLoadResponse.getMap() + " [" + mapLoadResponse.isInserted() + "]");
-                matchInProgress = TGM.get().getTeamClient().loadMatch(new MatchLoadRequest(mapLoadResponse.getMap()));
-                Bukkit.getLogger().info("Match successfully loaded [" + matchInProgress.getMap() + "]");
-            }
+
+        Bukkit.getScheduler().runTaskAsynchronously(TGM.get(), () -> {
+            MapLoadResponse mapLoadResponse = TGM.get().getTeamClient().loadmap(new Map(mapInfo.getName(), mapInfo.getVersion(), mapInfo.getAuthors(), mapInfo.getGametype().toString(), teams));
+            Bukkit.getLogger().info("Received load map response. Id: " + mapLoadResponse.getMap() + " [" + mapLoadResponse.isInserted() + "]");
+            matchInProgress = TGM.get().getTeamClient().loadMatch(new MatchLoadRequest(mapLoadResponse.getMap()));
+            Bukkit.getLogger().info("Match successfully loaded [" + matchInProgress.getMap() + "]");
         });
     }
 
     @EventHandler
     public void onKill(PlayerDeathEvent event) {
-        if (isStatsDisabled()) {
-            return;
-        }
+        if (isStatsDisabled()) return;
+        DeathModule module = deathModule.getPlayer(event.getPlayer());
 
-        PlayerContext player = TGM.get().getPlayerManager().getPlayerContext(event.getPlayer()); //dead
+        PlayerContext killed = TGM.get().getPlayerManager().getPlayerContext(module.getPlayer());
 
-        String playerItem = "";
-        if (event.getPlayer().getItemInHand() != null) {
-            playerItem = event.getPlayer().getItemInHand().getType().toString();
-        }
-
-        String killerItem = "";
+        String playerItem = event.getPlayer().getInventory().getItemInMainHand() == null ? "" : event.getPlayer().getInventory().getItemInMainHand().getType().toString();
+        String killerItem = module.getItem() == null ? "" : module.getItem().getType().toString();
         String killerId = null;
+
         if (event instanceof PlayerDeathByPlayerEvent) {
-            killerId = TGM.get().getPlayerManager().getPlayerContext(((PlayerDeathByPlayerEvent) event).getCause()).getUserProfile().getId().toString();
-            if (((PlayerDeathByPlayerEvent) event).getCause().getItemInHand() != null) {
-                killerItem = ((PlayerDeathByPlayerEvent) event).getCause().getItemInHand().getType().toString();
-            }
+            PlayerContext context = TGM.get().getPlayerManager().getPlayerContext(module.getKiller());
+
+            if (context == null) return;
+            killerId = context.getUserProfile().getId().toString();
         }
 
-        Death death = new Death(player.getUserProfile().getId().toString(), killerId, playerItem,
+        Death death = new Death(killed.getUserProfile().getId().toString(), killerId, playerItem,
                 killerItem, matchInProgress.getMap(), matchInProgress.getId());
-        Bukkit.getScheduler().runTaskAsynchronously(TGM.get(), new Runnable() {
-            @Override
-            public void run() {
-                TGM.get().getTeamClient().addKill(death);
-            }
-        });
+
+        Bukkit.getScheduler().runTaskAsynchronously(TGM.get(), () -> TGM.get().getTeamClient().addKill(death));
     }
 
     public boolean isStatsDisabled() {
