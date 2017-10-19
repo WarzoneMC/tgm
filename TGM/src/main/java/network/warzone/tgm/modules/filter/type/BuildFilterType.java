8b72360733194a7182a7f94166579b9854ac09f6
jorgeberrex <jorgeberrex@hotmail.com>
2017-10-19 16:19:14 -0500

    Disable pistons in protected regions

--- TGM/src/main/java/network/warzone/tgm/modules/filter/type/BuildFilterType.java
+++ TGM/src/main/java/network/warzone/tgm/modules/filter/type/BuildFilterType.java
@@ -6,11 +6,15 @@ import network.warzone.tgm.modules.region.Region;
 import network.warzone.tgm.modules.team.MatchTeam;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.ItemFrame;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPistonExtendEvent;
+import org.bukkit.event.block.BlockPistonRetractEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.hanging.HangingBreakEvent;
@@ -122,6 +126,50 @@ public class BuildFilterType implements FilterType, Listener {
             for (Region region : regions) {
                 if (region.contains(event.getEntity().getLocation())) {
                     event.setCancelled(true);
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPistonExtend(BlockPistonExtendEvent event) {
+        if (!event.isCancelled()) {
+            FilterResult filterResult = evaluator.evaluate();
+            if (filterResult == FilterResult.DENY) {
+                for (Region region : regions) {
+                    if (region.contains(event.getBlock().getLocation().clone().add(event.getDirection().getModX(), event.getDirection().getModY(), event.getDirection().getModZ()))) {
+                        event.setCancelled(true);
+                        return;
+                    } else {
+                        for (Block block : event.getBlocks()) {
+                            if (region.contains(event.getBlock().getLocation().clone().add(event.getDirection().getModX(), event.getDirection().getModY(), event.getDirection().getModZ())) || region.contains(block.getLocation().clone().add(event.getDirection().getModX(), event.getDirection().getModY(), event.getDirection().getModZ()))) {
+                                event.setCancelled(true);
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPistonRetract(BlockPistonRetractEvent event) {
+        if (!event.isCancelled()) {
+            FilterResult filterResult = evaluator.evaluate();
+            if (filterResult == FilterResult.DENY) {
+                for (Region region : regions) {
+                    if (region.contains(event.getBlock().getLocation().clone().add(event.getDirection().getOppositeFace().getModX(), event.getDirection().getOppositeFace().getModY(), event.getDirection().getOppositeFace().getModZ()))) {
+                        event.setCancelled(true);
+                        return;
+                    } else {
+                        for (Block block : event.getBlocks()) {
+                            if (region.contains(block.getLocation().clone().add(event.getDirection().getOppositeFace().getModX(), event.getDirection().getOppositeFace().getModY(), event.getDirection().getOppositeFace().getModZ()))) {
+                                event.setCancelled(true);
+                                return;
+                            }
+                        }
+                    }
                 }
             }
         }
