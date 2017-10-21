32a70556146b76021030c906f04402b06addaa48
Synconix <32807873+Synconix@users.noreply.github.com>
2017-10-20 21:02:01 -0700

    Update CycleCommands.java

--- TGM/src/main/java/network/warzone/tgm/command/CycleCommands.java
+++ TGM/src/main/java/network/warzone/tgm/command/CycleCommands.java
@@ -299,7 +299,7 @@ public class CycleCommands {
 
     @Command(aliases = "next", desc = "View the next map in the rotation")
     public static void next(CommandContext cmd, CommandSender sender) {
-        sender.sendMessage(ChatColor.YELLOW + "" + ChatColor.BOLD + "Next Map: " + ChatColor.GRAY + TGM.get().getMatchManager().getMapRotation().getNext().getMapInfo().getName());
+        sender.sendMessage(ChatColor.YELLOW + "Next Map: " + ChatColor.GRAY + TGM.get().getMatchManager().getMapRotation().getNext().getMapInfo().getName());
     }
     
     @Command(aliases = "mapinfo", desc = "View the map info for the current map")
