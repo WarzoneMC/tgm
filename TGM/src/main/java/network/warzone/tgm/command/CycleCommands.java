b1697ef852fc6071a0f86c9e2db4f7ea5a184715
Synconix <32807873+Synconix@users.noreply.github.com>
2017-10-22 15:35:42 -0400

    Update CycleCommands.java (#142)
    * Update CycleCommands.java
    
    * Update CycleCommands.java
    
    * Update CycleCommands.java
    
    * Update CycleCommands.java
    
    * Update CycleCommands.java
    
    * Update CycleCommands.java

--- TGM/src/main/java/network/warzone/tgm/command/CycleCommands.java
+++ TGM/src/main/java/network/warzone/tgm/command/CycleCommands.java
@@ -301,6 +301,12 @@ public class CycleCommands {
     public static void next(CommandContext cmd, CommandSender sender) {
         sender.sendMessage(ChatColor.YELLOW + "Next Map: " + ChatColor.GRAY + TGM.get().getMatchManager().getMapRotation().getNext().getMapInfo().getName());
     }
+    
+    @Command(aliases = "map", desc = "View the map info for the current map")
+    public static void map(CommandContext cmd, CommandSender sender) {
+        MapInfo info = TGM.get().getMatchManager().getMapRotation().getCurrent().getMapInfo();
+        sender.sendMessage(ChatColor.GRAY + "Currently playing " + ChatColor.YELLOW + info.getGametype + ChatColor.GRAY + " on map " + ChatColor.YELLOW + info.getName + ChatColor.GRAY + "by " + ChatColor.YELLOW + StringUtils.join(info.getAuthors(), ", ")());
+    }
 
     @Command(aliases = {"config"}, desc = "Edit the configuration", usage = "(stats)", min = 1)
     @CommandPermissions({"tgm.config"})
