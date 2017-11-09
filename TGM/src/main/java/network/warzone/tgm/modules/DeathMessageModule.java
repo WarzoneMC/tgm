bc6622c40f5097c5fe544df470e40cc7f594da84
Thrasilias <33469916+Thrasilias@users.noreply.github.com>
2017-11-09 21:14:37 +0100

    Added XP Give message
    Changed message "+1 XP" to "XP_PER_KILL"

--- TGM/src/main/java/network/warzone/tgm/modules/DeathMessageModule.java
+++ TGM/src/main/java/network/warzone/tgm/modules/DeathMessageModule.java
@@ -15,6 +15,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 
+import static network.warzone.warzoneapi.models.UserProfile.XP_PER_KILL;
 import static org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
 public class DeathMessageModule extends MatchModule implements Listener {
@@ -52,22 +53,25 @@ public class DeathMessageModule extends MatchModule implements Listener {
                     message = playerTeam.getColor() + module.getPlayerName() + ChatColor.GRAY + " was shot into the void by " +
                             killerTeam.getColor() + module.getKillerName() + ChatColor.GRAY;
                 else
-                    if (!module.getPlayerName().equals(module.getKillerName())) {
-                        message = playerTeam.getColor() + module.getPlayerName() + ChatColor.GRAY + " was thrown into the void by " +
-                                killerTeam.getColor() + module.getKillerName() + ChatColor.GRAY + " using " +
-                                itemToString(weapon);
-                    } else {
-                        message = playerTeam.getColor() + module.getPlayerName() + ChatColor.GRAY + " fell into the void";
-                    }
+                if (!module.getPlayerName().equals(module.getKillerName())) {
+                    message = playerTeam.getColor() + module.getPlayerName() + ChatColor.GRAY + " was thrown into the void by " +
+                            killerTeam.getColor() + module.getKillerName() + ChatColor.GRAY + " using " +
+                            itemToString(weapon);
+                    module.getKiller().sendMessage("§a§l+" + XP_PER_KILL + " §bXP §d| §7Killed " + playerTeam.getColor() + module.getPlayerName());
+                } else {
+                    message = playerTeam.getColor() + module.getPlayerName() + ChatColor.GRAY + " fell into the void";
+                }
             } else if (cause.equals(DamageCause.PROJECTILE)) {
                 int distance = ((Double) module.getKillerLocation().distance(module.getPlayerLocation())).intValue();
                 message = playerTeam.getColor() + module.getPlayerName() + ChatColor.GRAY + " was shot by " +
                         killerTeam.getColor() + module.getKillerName() + ChatColor.GRAY + " from " + distance + " blocks";
+                module.getKiller().sendMessage("§a§l+" + XP_PER_KILL + " §bXP §d| §7Killed " + playerTeam.getColor() + module.getPlayerName());
             } else {
                 if (!module.getPlayerName().equals(module.getKillerName())) {
                     message = playerTeam.getColor() + module.getPlayerName() + ChatColor.GRAY + " was killed by " +
                             killerTeam.getColor() + module.getKillerName() + ChatColor.GRAY + " using " +
                             (cause.equals(DamageCause.ENTITY_ATTACK) ? itemToString(weapon) : "the the environment");
+                    module.getKiller().sendMessage("§a§l+" + XP_PER_KILL + " §bXP §d| §7Killed " + playerTeam.getColor() + module.getPlayerName());
                 } else {
                     message = playerTeam.getColor() + module.getPlayerName() + ChatColor.GRAY + " died to the environment";
                 }
