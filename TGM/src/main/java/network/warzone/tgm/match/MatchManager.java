8b1a250a8f1bfd69c9bcebd8c297c529eb3dd555
Thrasilias <33469916+Thrasilias@users.noreply.github.com>
2017-11-10 21:30:34 +0100

    Changed gamemode to CREATIVE
    Changed this to gamemode CREATIVE
    for so far i found 0 bugs with this
    if you find a bug change it back to Gamemode.ADVENTURE

--- TGM/src/main/java/network/warzone/tgm/match/MatchManager.java
+++ TGM/src/main/java/network/warzone/tgm/match/MatchManager.java
@@ -53,7 +53,7 @@ public class MatchManager {
         for (MatchTeam matchTeam : TGM.get().getModule(TeamManagerModule.class).getTeams()) {
             if (!matchTeam.isSpectator()) {
                 matchTeam.getMembers().forEach(playerContext -> {
-                    playerContext.getPlayer().setGameMode(GameMode.ADVENTURE);
+                    playerContext.getPlayer().setGameMode(GameMode.CREATIVE); // so far 0% bugs if you find a buf change it back to Gamemode.ADVENTURE
                     playerContext.getPlayer().setAllowFlight(true);
                     playerContext.getPlayer().setVelocity(new Vector(playerContext.getPlayer().getVelocity().getX(),
                             playerContext.getPlayer().getVelocity().getZ() + 1.0, playerContext.getPlayer().getVelocity().getZ()));
