2c1d5cbed25c4e8dfb8499da85ceea908cfa7295
Lucas <502badgamer@gmail.com>
2018-09-10 14:08:44 -0500

    Remove unneeded vehicle event

--- TGM/src/main/java/network/warzone/tgm/modules/filter/type/LeaveFilterType.java
+++ TGM/src/main/java/network/warzone/tgm/modules/filter/type/LeaveFilterType.java
@@ -6,12 +6,9 @@ import network.warzone.tgm.modules.filter.FilterResult;
 import network.warzone.tgm.modules.filter.evaluate.FilterEvaluator;
 import network.warzone.tgm.modules.region.Region;
 import network.warzone.tgm.modules.team.MatchTeam;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.vehicle.VehicleMoveEvent;
 
 import java.util.List;
 
@@ -36,29 +33,6 @@ public class LeaveFilterType implements FilterType, Listener {
                             event.getPlayer().sendMessage(message);
                         } else if (filterResult == FilterResult.ALLOW) {
                             event.setCancelled(false);
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    @EventHandler
-    public void onBoatMove(VehicleMoveEvent event) {
-        if (event.getFrom().getX() == event.getTo().getX() && event.getFrom().getY() == event.getTo().getY() && event.getFrom().getZ() == event.getTo().getZ()) return;
-        for (Entity passenger : event.getVehicle().getPassengers()) {
-            if (passenger instanceof Player) {
-                Player player = (Player) passenger;
-                for (Region region : regions) {
-                    if (!region.contains(event.getTo())) {
-                        for (MatchTeam matchTeam : teams) {
-                            if (matchTeam.containsPlayer(player)) {
-                                FilterResult filterResult = evaluator.evaluate(player);
-                                if (filterResult == FilterResult.DENY) {
-                                    event.getVehicle().teleport(event.getFrom().add(event.getTo().subtract(event.getFrom()).toVector().normalize().multiply(-1)));
-                                    player.sendMessage(message);
-                                }
-                            }
                         }
                     }
                 }
