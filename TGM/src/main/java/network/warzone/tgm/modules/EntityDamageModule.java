e02d8b753c3c2f67d2510160f463b33fe994ef18
Lucas <502badgamer@gmail.com>
2018-09-11 10:55:43 -0500

    Send damaged player's health on arrow hit
    Closes #281

--- TGM/src/main/java/network/warzone/tgm/modules/EntityDamageModule.java
+++ TGM/src/main/java/network/warzone/tgm/modules/EntityDamageModule.java
@@ -1,6 +1,12 @@
 package network.warzone.tgm.modules;
 
+import network.warzone.tgm.TGM;
 import network.warzone.tgm.match.MatchModule;
+import network.warzone.tgm.modules.team.MatchTeam;
+import network.warzone.tgm.modules.team.TeamManagerModule;
+import network.warzone.tgm.user.PlayerContext;
+import org.bukkit.ChatColor;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Damageable;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -22,6 +28,11 @@ public class EntityDamageModule extends MatchModule implements Listener {
         ProjectileSource shooter = event.getEntity().getShooter();
         if (shooter != null && shooter instanceof Player && event.getHitEntity() instanceof Damageable) {
             ((Damageable) event.getHitEntity()).damage(0.01, (Player) shooter);
+            if (event.getHitEntity() instanceof Player) {
+                Player damaged = (Player) event.getHitEntity();
+                MatchTeam team = TGM.get().getModule(TeamManagerModule.class).getTeam(damaged);
+                ((Player) shooter).sendActionBar(team.getColor() + damaged.getName() + ChatColor.DARK_GRAY + " [" + ChatColor.WHITE + damaged.getHealth() + ChatColor.GRAY + "/" + damaged.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue() + ChatColor.DARK_GRAY + "]");
+            }
         }
 
     }
