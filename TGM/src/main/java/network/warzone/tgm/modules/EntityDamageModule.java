a0f1bac98834a1e25d97a540f912f939955d1ba2
Lucas <502badgamer@gmail.com>
2018-09-11 17:02:00 -0500

    Rearrange conditionals

--- TGM/src/main/java/network/warzone/tgm/modules/EntityDamageModule.java
+++ TGM/src/main/java/network/warzone/tgm/modules/EntityDamageModule.java
@@ -22,19 +22,20 @@ public class EntityDamageModule extends MatchModule implements Listener {
 
     @EventHandler
     public void onProjectileHit(ProjectileHitEvent event) {
+        ProjectileSource shooter = event.getEntity().getShooter();
+        if (!(shooter instanceof Player)) return;
+        if (event.getHitEntity() instanceof Player) {
+            Player damaged = (Player) event.getHitEntity();
+            MatchTeam team = TGM.get().getModule(TeamManagerModule.class).getTeam(damaged);
+            if (team != null && !team.isSpectator()) {
+                ((Player) shooter).sendActionBar(team.getColor() + damaged.getName() + ChatColor.DARK_GRAY + " [" + ChatColor.WHITE + (int)damaged.getHealth() + ChatColor.GRAY + "/" + damaged.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue() + ChatColor.DARK_GRAY + "]");
+            }
+        }
         if (event.getEntityType() != EntityType.FISHING_HOOK &&
             event.getEntityType() != EntityType.SNOWBALL &&
             event.getEntityType() != EntityType.EGG) return;
-        ProjectileSource shooter = event.getEntity().getShooter();
-        if (shooter != null && shooter instanceof Player && event.getHitEntity() instanceof Damageable) {
+        if (event.getHitEntity() instanceof Damageable) {
             ((Damageable) event.getHitEntity()).damage(0.01, (Player) shooter);
-            if (event.getHitEntity() instanceof Player) {
-                Player damaged = (Player) event.getHitEntity();
-                MatchTeam team = TGM.get().getModule(TeamManagerModule.class).getTeam(damaged);
-                if (team != null && !team.isSpectator()) {
-                    ((Player) shooter).sendActionBar(team.getColor() + damaged.getName() + ChatColor.DARK_GRAY + " [" + ChatColor.WHITE + (int)damaged.getHealth() + ChatColor.GRAY + "/" + damaged.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue() + ChatColor.DARK_GRAY + "]");
-                }
-            }
         }
 
     }
