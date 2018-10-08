77fe619628ff8975cbf76c4741ba0a0426ed5afd
Lucas <502badgamer@gmail.com>
2018-10-08 14:58:58 -0500

    Don't allow going over size

--- TGM/src/main/java/network/warzone/tgm/modules/SpectatorModule.java
+++ TGM/src/main/java/network/warzone/tgm/modules/SpectatorModule.java
@@ -298,6 +298,7 @@ public class SpectatorModule extends MatchModule implements Listener {
                           if (player.isOnline()) clicker.teleport(player);
                         });
                 i++;
+                if (i >= size) break;
               }
               teleportMenu.open(event.getPlayer());
             }
