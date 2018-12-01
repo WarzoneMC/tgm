2fa1b70cdebaf41f469e82e5cc00adfa143dae6a
Daniel <24366082+danielgulic@users.noreply.github.com>
2018-12-01 16:38:12 +1100

    remove unused import

--- TGM/src/main/java/network/warzone/tgm/modules/SpectatorModule.java
+++ TGM/src/main/java/network/warzone/tgm/modules/SpectatorModule.java
@@ -17,7 +17,6 @@ import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.*;
