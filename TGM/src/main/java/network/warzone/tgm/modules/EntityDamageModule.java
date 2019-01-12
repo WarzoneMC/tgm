39e533bd96be4fe6d5ebd56ec17b52910eccdbc4
ThatOneTqnk <nodeadbatteries@gmail.com>
2019-01-12 02:46:12 -0600

    Fixed Projectile dmg Indicator

--- TGM/src/main/java/network/warzone/tgm/modules/EntityDamageModule.java
+++ TGM/src/main/java/network/warzone/tgm/modules/EntityDamageModule.java
@@ -5,13 +5,17 @@ import network.warzone.tgm.match.MatchModule;
 import network.warzone.tgm.modules.team.MatchTeam;
 import network.warzone.tgm.modules.team.TeamManagerModule;
 import network.warzone.tgm.user.PlayerContext;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Damageable;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.projectiles.ProjectileSource;
 
@@ -21,16 +25,9 @@ import org.bukkit.projectiles.ProjectileSource;
 public class EntityDamageModule extends MatchModule implements Listener {
 
     @EventHandler
-    public void onProjectileHit(ProjectileHitEvent event) {
+    public void onProjectileHitt(ProjectileHitEvent event) {
         ProjectileSource shooter = event.getEntity().getShooter();
         if (!(shooter instanceof Player)) return;
-        if (event.getHitEntity() instanceof Player) {
-            Player damaged = (Player) event.getHitEntity();
-            MatchTeam team = TGM.get().getModule(TeamManagerModule.class).getTeam(damaged);
-            if (team != null && !team.isSpectator()) {
-                ((Player) shooter).sendActionBar(team.getColor() + damaged.getName() + ChatColor.DARK_GRAY + " [" + ChatColor.WHITE + (int)damaged.getHealth() + ChatColor.GRAY + "/" + damaged.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue() + ChatColor.DARK_GRAY + "]");
-            }
-        }
         if (event.getEntityType() != EntityType.FISHING_HOOK &&
             event.getEntityType() != EntityType.SNOWBALL &&
             event.getEntityType() != EntityType.EGG) return;
@@ -38,6 +35,17 @@ public class EntityDamageModule extends MatchModule implements Listener {
             ((Damageable) event.getHitEntity()).damage(0.01, (Player) shooter);
         }
 
+    }
+
+    @EventHandler
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        if(event.getCause() == EntityDamageEvent.DamageCause.PROJECTILE) {
+            ProjectileSource shooter = ((Projectile) event.getDamager()).getShooter();
+            if(!(shooter instanceof Player && event.getEntity() instanceof Player)) return;
+            Player damaged = (Player) event.getEntity();
+            MatchTeam team = TGM.get().getModule(TeamManagerModule.class).getTeam(damaged);
+            if(team != null && !team.isSpectator() && (damaged.getHealth() - event.getFinalDamage() >= 0))((Player) shooter).sendActionBar(team.getColor() + damaged.getName() + ChatColor.DARK_GRAY + " [" + ChatColor.WHITE + ((int)damaged.getHealth() - (int) event.getFinalDamage()) + ChatColor.GRAY + "/" + damaged.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue() + ChatColor.DARK_GRAY + "]");
+        }
     }
 
 }
