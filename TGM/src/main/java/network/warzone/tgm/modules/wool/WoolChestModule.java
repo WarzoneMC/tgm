f8ce603b3dace702ec1c67c7a3111069ece4c50f
ThatOneTqnk <nodeadbatteries@gmail.com>
2019-01-13 01:48:18 -0600

    remove unused import

--- TGM/src/main/java/network/warzone/tgm/modules/wool/WoolChestModule.java
+++ TGM/src/main/java/network/warzone/tgm/modules/wool/WoolChestModule.java
@@ -9,7 +9,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.DyeColor;
 import org.bukkit.Material;
 import org.bukkit.Location;
-import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Chest;
 import org.bukkit.block.DoubleChest;
