f2abe21520f93b7ecd3b7145eea9aa51c3cfbdb3
Henry <pixelificz@gmail.com>
2019-05-15 18:48:25 -0400

    Allow users who join a team before the match starts to enter the enemy spawn

--- TGM/src/main/java/network/warzone/tgm/modules/filter/type/EnterFilterType.java
+++ TGM/src/main/java/network/warzone/tgm/modules/filter/type/EnterFilterType.java
@@ -21,16 +21,18 @@ public class EnterFilterType implements FilterType, Listener {
 
     @EventHandler
     public void onMove(PlayerMoveEvent event) {
-        for (Region region : regions) {
-            if (region.contains(event.getTo())) {
-                for (MatchTeam matchTeam : teams) {
-                    if (matchTeam.containsPlayer(event.getPlayer())) {
-                        FilterResult filterResult = evaluator.evaluate(event.getPlayer());
-                        if (filterResult == FilterResult.DENY) {
-                            event.setCancelled(true);
-                            event.getPlayer().sendMessage(message);
-                        } else if (filterResult == FilterResult.ALLOW) {
-                            event.setCancelled(false);
+        if (TGM.get().getMatchManager().getMatch().getMatchStatus() == MatchStatus.MID)
+            for (Region region : regions) {
+                if (region.contains(event.getTo())) {
+                    for (MatchTeam matchTeam : teams) {
+                        if (matchTeam.containsPlayer(event.getPlayer())) {
+                            FilterResult filterResult = evaluator.evaluate(event.getPlayer());
+                            if (filterResult == FilterResult.DENY) {
+                                event.setCancelled(true);
+                                event.getPlayer().sendMessage(message);
+                            } else if (filterResult == FilterResult.ALLOW) {
+                                event.setCancelled(false);
+                            }
                         }
                     }
                 }
