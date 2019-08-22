0eb25d1d49ba289f1e97ea6691b8e7cd231ec95a
draem <owen@gimbel.ca>
2019-08-22 15:32:38 -0400

    mess with kb some more

--- TGM/src/main/java/network/warzone/tgm/modules/LegacyDamageModule.java
+++ TGM/src/main/java/network/warzone/tgm/modules/LegacyDamageModule.java
@@ -2,10 +2,9 @@ package network.warzone.tgm.modules;
 
 import network.warzone.tgm.match.MatchModule;
 import org.bukkit.Material;
-import org.bukkit.entity.Arrow;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -46,7 +45,7 @@ public class LegacyDamageModule extends MatchModule implements Listener {
         if (event.getDamager() instanceof Arrow) {
             Entity e = event.getEntity();
             Arrow arrow = (Arrow) event.getDamager();
-            event.setDamage(event.getDamage());
+            event.setDamage(event.getDamage() / 3);
             addVelocity(e, arrow.getVelocity().normalize().multiply(0.85f));
         }
 
@@ -56,19 +55,46 @@ public class LegacyDamageModule extends MatchModule implements Listener {
     }
 
     private void applyKnockback(LivingEntity attacker, Player victim) {
-        Vector kb = attacker.getLocation().getDirection().setY(0).normalize().multiply(0.65f);
-        victim.setVelocity(magicKnockbackFunction(kb));
+        //Vector kb = attacker.getLocation().getDirection().setY(0).normalize().multiply(0.65f);
+        //victim.setVelocity(magicKnockbackFunction(kb));
+        victim.setVelocity(magicKnockbackFunction(attacker.getLocation().getDirection()));
     }
 
     private void addVelocity(Entity e, Vector vel) {
         e.setVelocity(e.getVelocity().add(vel));
     }
 
+    private void applyClijmartKnockback(LivingEntity damaged, Entity damager) {
+        double f = 0.5f;
+        if (damager instanceof Player) {
+            f += 0.3375 * ((Player) damager).getInventory().getItemInMainHand().getEnchantmentLevel(Enchantment.KNOCKBACK);
+        }
+        if (damager instanceof Arrow) {
+            f += 0.3375 * ((Arrow) damager).getKnockbackStrength();
+        }
+        f *= Math.max(0, 1 - damaged.getAttribute(Attribute.GENERIC_KNOCKBACK_RESISTANCE).getValue());
+        double d0 = damager instanceof Projectile ? -damager.getVelocity().getX() : damager.getLocation().getX() - damaged.getLocation().getX();
+        double d1 = damager instanceof Projectile ? -damager.getVelocity().getZ() : damager.getLocation().getZ() - damaged.getLocation().getZ();
+
+        double f1 = Math.sqrt(d0 * d0 + d1 * d1);
+        double motX = damaged.getVelocity().getX();
+        double motY = damaged.getVelocity().getY();
+        double motZ = damaged.getVelocity().getZ();
+        motX /= 2;
+        motY /= 2;
+        motZ /= 2;
+        motX -= d0 / f1 * f;
+        motY += f;
+        motZ -= d1 / f1 * f;
+        motY = Math.min(0.5, motY);
+        damaged.setVelocity(new Vector(Double.isFinite(motX) ? motX : 0, Double.isFinite(motY) ? motY : 0, Double.isFinite(motZ) ? motZ : 0));
+    }
+
     private Vector magicKnockbackFunction(Vector delta) {
         delta = delta.clone();
         delta.setY(0);
         delta.normalize();
-        final double theta = Math.toRadians(0.5);
+        final double theta = Math.toRadians(3);
         final double cos = Math.cos(theta);
         delta.setX(cos * delta.getX());
         delta.setY(Math.sin(theta));
