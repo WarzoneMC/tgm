2967324d89a9c9c5756125058369eca2f70d4535
Draem <23418831+Draem@users.noreply.github.com>
2019-08-23 12:21:55 -0400

    Try a different system

--- TGM/src/main/java/network/warzone/tgm/modules/scoreboard/ScoreboardManagerModule.java
+++ TGM/src/main/java/network/warzone/tgm/modules/scoreboard/ScoreboardManagerModule.java
@@ -84,8 +84,8 @@ public class ScoreboardManagerModule extends MatchModule implements Listener {
 
     public Team registerScoreboardTeam(SimpleScoreboard simpleScoreboard, MatchTeam matchTeam, PlayerContext playerContext) {
         Team team = simpleScoreboard.getScoreboard().registerNewTeam(matchTeam.getId());
-        team.setPrefix(matchTeam.getColor().toString());
-        //team.setColor(matchTeam.getColor());
+        //team.setPrefix(matchTeam.getColor().toString());
+        team.setColor(matchTeam.getColor());
         team.setCanSeeFriendlyInvisibles(false); // Fixes anti cheat entity visible when it shouldn't be
         team.setAllowFriendlyFire(false);
         team.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
