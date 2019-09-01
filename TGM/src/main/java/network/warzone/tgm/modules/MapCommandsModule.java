3234d223cdc3bbf239a8c72f064902564992fd58
Daniel Gulic <danielgulic@gmail.com>
2019-09-01 14:56:10 +1000

    ability to specify command to be run on match start in map.json

---
+++ TGM/src/main/java/network/warzone/tgm/modules/MapCommandsModule.java
@@ -0,0 +1,25 @@
+package network.warzone.tgm.modules;
+
+import network.warzone.tgm.TGM;
+import network.warzone.tgm.match.Match;
+import network.warzone.tgm.match.MatchModule;
+
+public class MapCommandsModule extends MatchModule {
+    Match match;
+
+    String startCommand;
+
+    @Override
+    public void enable() {
+        if (this.startCommand != null)
+            TGM.get().getServer().dispatchCommand(TGM.get().getServer().getConsoleSender(), this.startCommand);
+    }
+
+    @Override
+    public void load(Match match) {
+        this.match = match;
+
+        if (match.getMapContainer().getMapInfo().getJsonObject().has("startCommand"))
+            this.startCommand = match.getMapContainer().getMapInfo().getJsonObject().get("startCommand").getAsString();
+    }
+}
