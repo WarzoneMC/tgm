f5b60b49ee7662cdabe6f7bdecb5306ea2be5205
jorgeberrex <jorgeberrex@hotmail.com>
2019-09-14 21:56:32 -0500

    Why didn't this commit

--- TGM/src/main/java/network/warzone/tgm/modules/kit/parser/EffectKitNodeParser.java
+++ TGM/src/main/java/network/warzone/tgm/modules/kit/parser/EffectKitNodeParser.java
@@ -3,9 +3,7 @@ package network.warzone.tgm.modules.kit.parser;
 import com.google.gson.JsonObject;
 import network.warzone.tgm.modules.kit.KitNode;
 import network.warzone.tgm.modules.kit.types.EffectKitNode;
-import network.warzone.tgm.util.Strings;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
+import network.warzone.tgm.parser.effect.EffectParser;
 
 import java.util.Collections;
 import java.util.List;
@@ -14,37 +12,7 @@ public class EffectKitNodeParser implements KitNodeParser {
 
     @Override
     public List<KitNode> parse(JsonObject jsonObject) {
-        return Collections.singletonList(new EffectKitNode(parsePotionEffect(jsonObject)));
+        return Collections.singletonList(new EffectKitNode(EffectParser.parse(jsonObject)));
     }
 
-    public static PotionEffect parsePotionEffect(JsonObject jsonObject) {
-        PotionEffectType type = PotionEffectType.getByName(Strings.getTechnicalName(jsonObject.get("type").getAsString()));
-
-
-        int duration = 30;
-        int amplifier = 0;
-        boolean ambient = true;
-        boolean particles = true;
-        //Color color = null;
-
-        if (jsonObject.has("duration")) { // Ticks
-            duration = jsonObject.get("duration").getAsInt();
-        }
-
-        if (jsonObject.has("amplifier")) {
-            amplifier = jsonObject.get("amplifier").getAsInt();
-        }
-
-        if (jsonObject.has("ambient")) {
-            ambient = jsonObject.get("ambient").getAsBoolean();
-        }
-
-        if (jsonObject.has("particles")) {
-            particles = jsonObject.get("particles").getAsBoolean();
-        }
-
-        //if (jsonObject.has("color")) color = Color.jsonObject.get("color")
-
-        return new PotionEffect(type, duration, amplifier, ambient, particles, false);
-    }
 }
