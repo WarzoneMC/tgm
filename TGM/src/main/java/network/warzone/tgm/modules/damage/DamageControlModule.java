d0c2759fd4401cfe1977d40b58cb45d0e2fb3e92
jorgeberrex <jorgeberrex@hotmail.com>
2019-10-01 21:07:20 -0500

    allow untechnical damage cause names

--- TGM/src/main/java/network/warzone/tgm/modules/damage/DamageControlModule.java
+++ TGM/src/main/java/network/warzone/tgm/modules/damage/DamageControlModule.java
@@ -1,14 +1,17 @@
 package network.warzone.tgm.modules.damage;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import network.warzone.tgm.match.Match;
 import network.warzone.tgm.match.MatchModule;
+import network.warzone.tgm.util.Strings;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import static org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
@@ -21,8 +24,11 @@ public class DamageControlModule extends MatchModule implements Listener {
         JsonObject matchObj = match.getMapContainer().getMapInfo().getJsonObject();
         if (matchObj.has("damageControl")) {
             JsonObject damageControlObj = matchObj.getAsJsonObject("damageControl");
-            for (DamageCause cause : DamageCause.values()) {
-                enabled.put(cause, !damageControlObj.has(cause.name()) || damageControlObj.get(cause.name()).getAsBoolean());
+            for (Map.Entry<String, JsonElement> entry : damageControlObj.entrySet()) {
+                if (!entry.getValue().isJsonPrimitive()) continue;
+                DamageCause cause = DamageCause.valueOf(Strings.getTechnicalName(entry.getKey()));
+                if (cause == null) continue;
+                enabled.put(cause, entry.getValue().getAsBoolean());
             }
         }
     }
