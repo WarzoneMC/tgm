d5d4322cda85264f18043461c1f83c0bddcb31fe
Owehttamy <47429979+Owehttamy@users.noreply.github.com>
2019-10-02 09:03:27 -0500

    Create WinsLeaderboardResponse.java

---
+++ WarzoneAPI/src/main/java/network/warzone/warzoneapi/models/WinsLeaderboardResponse.java
@@ -0,0 +1,10 @@
+package network.warzone.warzoneapi.models;
+
+import java.util.ArrayList;
+
+/**
+ * Copied off of Daniel's work from 9/12/2018 but changed it a little bit so the teacher wouldn't notice on 10/2/2019
+ */
+public class WinsLeaderboardResponse extends ArrayList<UserProfile> {
+
+}
