e96222aefe1c06be9a061425a959385a851fc839
ThatOneTqnk <nodeadbatteries@gmail.com>
2019-10-03 11:51:18 -0500

    time to teleport ASYNCHRONOUSLY

--- TGM/src/main/java/network/warzone/tgm/match/MatchManager.java
+++ TGM/src/main/java/network/warzone/tgm/match/MatchManager.java
@@ -112,7 +112,7 @@ public class MatchManager {
 
         // Transport all players to the new world so we can unload the old one.
         Bukkit.getOnlinePlayers().forEach(player ->
-                player.teleport(world.getSpawnLocation(), PlayerTeleportEvent.TeleportCause.PLUGIN));
+                player.teleportAsync(world.getSpawnLocation(), PlayerTeleportEvent.TeleportCause.PLUGIN));
 
         //create and load the match.
         Match createdMatch = new Match(matchUuid, matchManifest, world, mapContainer);
@@ -127,15 +127,17 @@ public class MatchManager {
         //if a match is currently running, unload it.
         if (oldMatch != null) {
             oldMatch.getWorld().getPlayers().forEach(player ->
-                    player.teleport(world.getSpawnLocation(), PlayerTeleportEvent.TeleportCause.PLUGIN));
+                    player.teleportAsync(world.getSpawnLocation(), PlayerTeleportEvent.TeleportCause.PLUGIN));
 
             TGM.get().getLogger().info("Unloading match: " + oldMatch.getUuid().toString() + " (File: " + oldMatch.getWorld().getWorldFolder().getPath() + ")");
 
             boolean save = TGM.get().getConfig().getBoolean("map.save-matches", false);
             Bukkit.unloadWorld(oldMatch.getWorld(), save);
+            Bukkit.broadcastMessage(".unloading: " + save);
             if (!save)
                 Bukkit.getScheduler().runTaskLaterAsynchronously(TGM.get(), () -> {
                     try {
+                        Bukkit.broadcastMessage(oldMatch.getWorld().getWorldFolder().toString());
                         FileUtils.deleteDirectory(oldMatch.getWorld().getWorldFolder());
                     } catch (IOException e) {
                         e.printStackTrace();
