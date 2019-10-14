81c2c4d2ef15350f3fd850d1a94c288c93ae0ab5
jorgeberrex <jorgeberrex@hotmail.com>
2019-10-13 21:45:54 -0500

    new death message system

--- TGM/src/main/java/network/warzone/tgm/modules/death/DeathMessageModule.java
+++ TGM/src/main/java/network/warzone/tgm/modules/death/DeathMessageModule.java
@@ -1,5 +1,7 @@
 package network.warzone.tgm.modules.death;
 
+import lombok.Getter;
+import lombok.Setter;
 import network.warzone.tgm.match.Match;
 import network.warzone.tgm.match.MatchModule;
 import network.warzone.tgm.modules.team.MatchTeam;
@@ -16,11 +18,23 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.*;
+
 import static org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
 public class DeathMessageModule extends MatchModule implements Listener {
 
     private DeathModule deathModule;
+
+    @Getter @Setter
+    private DeathMessageEvaluator defaultDeathMessage = (d) -> {
+        broadcastDeathMessage(d.player, d.killer, "%s%s%s died to the environment",
+            d.playerTeam.getColor(),
+            d.player.getName(),
+            ChatColor.GRAY
+        );
+        return true;
+    };
 
     public void load(Match match) {
         deathModule = match.getModule(DeathModule.class);
@@ -32,95 +46,17 @@ public class DeathMessageModule extends MatchModule implements Listener {
 
         if (deathInfo.playerTeam.isSpectator()) return; //stupid spectators
 
-        String message;
-        ItemStack weapon = deathInfo.item;
-        DamageCause cause = deathInfo.cause;
-
-        MatchTeam playerTeam = deathInfo.playerTeam;
-        MatchTeam killerTeam = deathInfo.killerTeam;
-
-        if (deathInfo.killer != null && deathInfo.killerName != null) {
-            if (cause.equals(DamageCause.FALL)) {
-                if (weapon != null && weapon.getType().equals(Material.BOW))
-                    message = playerTeam.getColor() + deathInfo.playerName + ChatColor.GRAY + " was shot off a high place by " +
-                            killerTeam.getColor() + deathInfo.killerName + ChatColor.GRAY;
-                else
-                    message = playerTeam.getColor() + deathInfo.playerName + ChatColor.GRAY + " was thrown off a high place by " +
-                            killerTeam.getColor() + deathInfo.killerName + ChatColor.GRAY + " using " +
-                            ItemUtils.itemToString(weapon);
-            } else if (cause.equals(DamageCause.VOID)) {
-                if (weapon != null && (weapon.getType().equals(Material.BOW) || weapon.getType().equals(Material.TRIDENT)))
-                    message = playerTeam.getColor() + deathInfo.playerName + ChatColor.GRAY + " was shot into the void by " +
-                            killerTeam.getColor() + deathInfo.killerName + ChatColor.GRAY;
-                else
-                    if (!deathInfo.playerName.equals(deathInfo.killerName)) {
-                        message = playerTeam.getColor() + deathInfo.playerName + ChatColor.GRAY + " was thrown into the void by " +
-                                killerTeam.getColor() + deathInfo.killerName + ChatColor.GRAY + " using " +
-                                ItemUtils.itemToString(weapon);
-                    } else {
-                        message = playerTeam.getColor() + deathInfo.playerName + ChatColor.GRAY + " fell into the void";
-                    }
-            } else if (cause.equals(DamageCause.PROJECTILE)) {
-                int distance = ((Double) deathInfo.killerLocation.distance(deathInfo.playerLocation)).intValue();
-                message = playerTeam.getColor() + deathInfo.playerName + ChatColor.GRAY + (weapon.getType() == Material.TRIDENT ? " forked " : " was shot by ") +
-                        killerTeam.getColor() + deathInfo.killerName + ChatColor.GRAY + " from " + distance + (distance == 1 ? " block" : " blocks");
-            } else if (cause.equals(DamageCause.FIRE) || cause.equals(DamageCause.FIRE_TICK)) {
-                if (!deathInfo.playerName.equals(deathInfo.killerName)) {
-                    message = playerTeam.getColor() + deathInfo.playerName + ChatColor.GRAY + " was burned to death by " + killerTeam.getColor() + deathInfo.killerName +ChatColor.GRAY;
-                } else {
-                    message = playerTeam.getColor() + deathInfo.playerName + ChatColor.GRAY + " burned to death";
-                }
-            } else {
-                if (!deathInfo.playerName.equals(deathInfo.killerName)) {
-                    message = playerTeam.getColor() + deathInfo.playerName + ChatColor.GRAY + " was killed by " +
-                            killerTeam.getColor() + deathInfo.killerName + ChatColor.GRAY + " using " +
-                            (cause.equals(DamageCause.ENTITY_ATTACK) ? ItemUtils.itemToString(weapon) : "the environment");
-                } else {
-                    message = playerTeam.getColor() + deathInfo.playerName + ChatColor.GRAY + " died to the environment";
-                }
-            }
-
-            if (deathInfo.killer != null) deathInfo.killer.playSound(deathInfo.killer.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 3, 1.4f);
-        } else {
-            if (cause.equals(DamageCause.FALL)) {
-                message = playerTeam.getColor() + deathInfo.playerName + ChatColor.GRAY + " fell from a high place";
-            } else if (cause.equals(DamageCause.VOID)) {
-                message = playerTeam.getColor() + deathInfo.playerName + ChatColor.GRAY + " fell into the void";
-            } else if (cause.equals(DamageCause.FIRE) || cause.equals(DamageCause.FIRE_TICK)) {
-                message = playerTeam.getColor() + deathInfo.playerName + ChatColor.GRAY + " burned to death";
-            } else {
-                message = playerTeam.getColor() + deathInfo.playerName + ChatColor.GRAY + " died to the environment";
-            }
+        for (DeathMessageEvaluator messageEvaluator : this.deathMessages.getOrDefault(event.getCause(), Collections.singletonList(defaultDeathMessage))) {
+            if (messageEvaluator.evaluate(deathInfo)) break;
         }
 
         deathInfo.player.getWorld().playSound(deathInfo.playerLocation, Sound.ENTITY_IRON_GOLEM_DEATH, 2, 2);
-
-        if (message.length() > 0) {
-            broadcastDeathMessage(deathInfo.player, deathInfo.killer, message);
-
-            deathInfo.killer = null;
-            deathInfo.killerName = null;
-        }
-
+        if (deathInfo.killer != null) deathInfo.killer.playSound(deathInfo.killer.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 3, 1.4f);
     }
 
-    private void broadcastDeathMessage(Player dead, Player killer, String message) {
+    public static void broadcastDeathMessage(Player dead, Player killer, String message, Object... args) {
+        message = String.format(message, args);
         for (Player player : Bukkit.getOnlinePlayers()) {
-
-            /* TODO make look better and also fix
-            //bold messages when the player is involved
-            if (dead == player || (killer != null && killer == player)) {
-                message = message.replaceAll(dead.getName() + ChatColor.GRAY, ChatColor.BOLD + dead.getName() + ChatColor.GRAY + ChatColor.BOLD);
-                if (killer != null) {
-                    if (message.contains(killer.getName() + ChatColor.GRAY)) {
-                        message = message.replaceAll(killer.getName() + ChatColor.GRAY, ChatColor.BOLD + killer.getName() + ChatColor.GRAY + ChatColor.BOLD);
-                    } else {
-                        message = message.replaceAll(killer.getName(), ChatColor.BOLD + killer.getName());
-                    }
-                }
-            }
-            */
-
             player.getPlayer().sendMessage(message);
         }
     }
@@ -130,6 +66,162 @@ public class DeathMessageModule extends MatchModule implements Listener {
     public void onBukkitDeath(PlayerDeathEvent event) {
         event.setDeathMessage("");
     }
+
+    private Map<DamageCause, List<DeathMessageEvaluator>> deathMessages = new LinkedHashMap<DamageCause, List<DeathMessageEvaluator>>() {{
+        put(DamageCause.ENTITY_ATTACK, Arrays.asList(
+                (d) -> {
+                    if (d.killer != null) return false;
+                    broadcastDeathMessage(d.player, null, "%s%s%s died",
+                            d.playerTeam.getColor(),
+                            d.playerName,
+                            ChatColor.GRAY
+                    );
+                    return true;
+                },
+                (d) -> {
+                    broadcastDeathMessage(d.player, d.killer, "%s%s%s was killed by %s%s%s using %s",
+                            d.playerTeam.getColor(),
+                            d.playerName,
+                            ChatColor.GRAY,
+                            d.killerTeam.getColor(),
+                            d.killerName,
+                            ChatColor.GRAY,
+                            (d.cause.equals(DamageCause.ENTITY_ATTACK) ? ItemUtils.itemToString(d.item) : "the environment")
+
+                    );
+                    return true;
+                }
+        ));
+        put(DamageCause.ENTITY_SWEEP_ATTACK, get(DamageCause.ENTITY_ATTACK));
+        put(DamageCause.FALL, Arrays.asList(
+                (d) -> {
+                    if (d.killer != null) return false;
+                    broadcastDeathMessage(d.player, null, "%s%s%s fell from a high place",
+                            d.playerTeam.getColor(),
+                            d.playerName,
+                            ChatColor.GRAY
+                    );
+                    return true;
+                },
+                (d) -> {
+                    if (d.item == null || !d.item.getType().equals(Material.BOW)) return false;
+                    broadcastDeathMessage(d.player, d.killer, "%s%s%s was shot off a high place by %s%s",
+                            d.playerTeam.getColor(),
+                            d.playerName,
+                            ChatColor.GRAY,
+                            d.killerTeam.getColor(),
+                            d.killerName
+                    );
+                    return true;
+                },
+                (d) -> {
+                    broadcastDeathMessage(d.player, d.killer, "%s%s%s was thrown off a high place by %s%s%s using %s",
+                            d.playerTeam.getColor(),
+                            d.playerName,
+                            ChatColor.GRAY,
+                            d.killerTeam.getColor(),
+                            d.killerName,
+                            ChatColor.GRAY,
+                            ItemUtils.itemToString(d.item)
+                    );
+                    return true;
+                }
+        ));
+        put(DamageCause.VOID, Arrays.asList(
+                (d) -> {
+                    if (d.killer != null) return false;
+                    broadcastDeathMessage(d.player, null, "%s%s%s fell into the void",
+                            d.playerTeam.getColor(),
+                            d.playerName,
+                            ChatColor.GRAY
+                    );
+                    return true;
+                },
+                (d) -> {
+                    if (d.item == null || !d.item.getType().equals(Material.BOW)) return false;
+                    broadcastDeathMessage(d.player, d.killer, "%s%s%s was shot into the void by %s%s",
+                            d.playerTeam.getColor(),
+                            d.playerName,
+                            ChatColor.GRAY,
+                            d.killerTeam.getColor(),
+                            d.killerName
+                    );
+                    return true;
+                },
+                (d) -> {
+                    broadcastDeathMessage(d.player, d.killer, "%s%s%s was thrown into the void by %s%s%s using %s",
+                            d.playerTeam.getColor(),
+                            d.playerName,
+                            ChatColor.GRAY,
+                            d.killerTeam.getColor(),
+                            d.killerName,
+                            ChatColor.GRAY,
+                            ItemUtils.itemToString(d.item)
+                    );
+                    return true;
+                }
+        ));
+        put(DamageCause.PROJECTILE, Arrays.asList(
+                (d) -> {
+                    if (d.killer != null) return false;
+                    broadcastDeathMessage(d.player, null, "%s%s%s was %s to death",
+                            d.playerTeam.getColor(),
+                            d.playerName,
+                            ChatColor.GRAY,
+                            (d.item.getType() == Material.TRIDENT ? "forked" : "shot")
+                    );
+                    return true;
+                },
+                (d) -> {
+                    if (d.item == null) return false;
+                    int distance = ((Double) d.killerLocation.distance(d.playerLocation)).intValue();
+                    broadcastDeathMessage(d.player, d.killer, "%s%s%s was %s by %s%s%s from %d%s",
+                            d.playerTeam.getColor(),
+                            d.playerName,
+                            ChatColor.GRAY,
+                            (d.item.getType() == Material.TRIDENT ? "forked" : "shot"),
+                            d.killerTeam.getColor(),
+                            d.killerName,
+                            ChatColor.GRAY,
+                            distance,
+                            (distance == 1 ? " block" : " blocks")
+                    );
+                    return true;
+                }
+        ));
+        put(DamageCause.FIRE, Arrays.asList(
+                (d) -> {
+                    if (d.killer != null) return false;
+                    broadcastDeathMessage(d.player, null, "%s%s%s burned to death",
+                            d.playerTeam.getColor(),
+                            d.playerName,
+                            ChatColor.GRAY
+                    );
+                    return true;
+                },
+                (d) -> {
+                    broadcastDeathMessage(d.player, d.killer, "%s%s%s was burned to death by %s%s",
+                            d.playerTeam.getColor(),
+                            d.playerName,
+                            ChatColor.GRAY,
+                            d.killerTeam.getColor(),
+                            d.killerName
+                    );
+                    return true;
+                }
+        ));
+        put(DamageCause.FIRE_TICK, get(DamageCause.FIRE));
+        put(DamageCause.SUFFOCATION, Arrays.asList(
+                (d) -> {
+                    broadcastDeathMessage(d.player, null, "%s%s%s suffocated to death",
+                            d.playerTeam.getColor(),
+                            d.playerName,
+                            ChatColor.GRAY
+                    );
+                    return true;
+                })
+        );
+    }};
 
 
 }
