8f67c4cee753027cf3310fd438c2e74a70f6c20e
AtditC <ardit.citaku001@gmail.com>
2019-10-30 14:26:21 +0100

    Update punishment broadcast
    Updates the punishment broadcast so whenever a nicked staff member punishes a player, it will broadcast the original name. Has been tested, works fine.

--- TGM/src/main/java/network/warzone/tgm/nickname/NickManager.java
+++ TGM/src/main/java/network/warzone/tgm/nickname/NickManager.java
@@ -16,6 +16,7 @@ import network.warzone.tgm.modules.team.TeamManagerModule;
 import network.warzone.tgm.modules.visibility.VisibilityController;
 import network.warzone.tgm.modules.visibility.VisibilityControllerImpl;
 import network.warzone.tgm.user.PlayerContext;
+import network.warzone.tgm.util.HashMaps;
 import network.warzone.warzoneapi.models.MojangProfile;
 import network.warzone.warzoneapi.models.Rank;
 import network.warzone.warzoneapi.models.Skin;
@@ -63,6 +64,15 @@ public class NickManager {
 
             queuedNicks.add(new QueuedNick(newName, skin, player));
         });
+    }
+
+    public String getOriginalName(String username) {
+        if (nickNames.containsValue(username)) {
+            UUID uuid = HashMaps.reverseGetFirst(username, nickNames);
+            return originalNames.get(uuid);
+        } else {
+            return username;
+        }
     }
 
     public Optional<QueuedNick> getQueuedNick(Player player) {
