adbd6054a6980c0922dda5bebaf75b7297cb7063
Daniel <danielgulic@gmail.com>
2019-12-14 12:36:15 +1100

    1.15 support

--- TGM/src/main/java/network/warzone/tgm/parser/item/meta/ItemBookParser.java
+++ TGM/src/main/java/network/warzone/tgm/parser/item/meta/ItemBookParser.java
@@ -26,11 +26,11 @@ public class ItemBookParser implements ItemMetaParser {
 
     static {
         try {
-            //classIChatBaseComponent = Class.forName("net.minecraft.server.v1_14_R1.IChatBaseComponent");
-            classChatSerializer = Class.forName("net.minecraft.server.v1_14_R1.IChatBaseComponent$ChatSerializer");
+            //classIChatBaseComponent = Class.forName("net.minecraft.server.v1_15_R1.IChatBaseComponent");
+            classChatSerializer = Class.forName("net.minecraft.server.v1_15_R1.IChatBaseComponent$ChatSerializer");
             methodA = classChatSerializer.getDeclaredMethod("a", String.class);
 
-            classCraftMetaBook = Class.forName("org.bukkit.craftbukkit.v1_14_R1.inventory.CraftMetaBook");
+            classCraftMetaBook = Class.forName("org.bukkit.craftbukkit.v1_15_R1.inventory.CraftMetaBook");
             fieldPages = classCraftMetaBook.getDeclaredField("pages");
 
         } catch (ClassNotFoundException | NoSuchFieldException | NoSuchMethodException e) {
