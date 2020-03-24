23ce9491c5d989a6002249e84842838bb3634532
jorgeberrex <jorgeberrex@hotmail.com>
2020-03-23 23:27:03 -0500

    convert CanPlaceOn and CanDestroy materials to technical names

--- TGM/src/main/java/network/warzone/tgm/parser/item/meta/ItemCanPlaceOnParser.java
+++ TGM/src/main/java/network/warzone/tgm/parser/item/meta/ItemCanPlaceOnParser.java
@@ -3,6 +3,7 @@ package network.warzone.tgm.parser.item.meta;
 import com.destroystokyo.paper.Namespaced;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import network.warzone.tgm.util.Strings;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -23,7 +24,7 @@ public class ItemCanPlaceOnParser implements ItemMetaParser {
                 if (!element.isJsonPrimitive()) {
                     continue;
                 }
-                keys.add(NamespacedKey.minecraft(element.getAsString()));
+                keys.add(NamespacedKey.minecraft(Strings.getTechnicalName(element.getAsString())));
             }
             meta.setPlaceableKeys(keys);
         }
