b7b3eeb8341fcf6d80404fa4ed79b927d32b16ac
chatasma <nodeadbatteries@gmail.com>
2021-01-09 00:28:57 -0600

    Rotation map shuffling

--- TGM/src/main/java/network/warzone/tgm/map/Rotation.java
+++ TGM/src/main/java/network/warzone/tgm/map/Rotation.java
@@ -1,15 +1,36 @@
 package network.warzone.tgm.map;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-@AllArgsConstructor
-@Getter
 public class Rotation {
-    private final String name;
-    private final boolean isDefault;
-    private final RotationRequirement requirements;
-    private final List<MapContainer> maps;
+    @Getter private final String name;
+    @Getter private final boolean isDefault;
+    @Getter private final RotationRequirement requirements;
+    private final List<MapContainer> baseMaps;
+    private List<MapContainer> activeMaps;
+
+    public Rotation(final String name, final boolean isDefault, final RotationRequirement requirements, List<MapContainer> baseMaps) {
+        this.name = name;
+        this.isDefault = isDefault;
+        this.requirements = requirements;
+        this.baseMaps = baseMaps;
+        this.activeMaps = new ArrayList<>(baseMaps);
+    }
+
+    public List<MapContainer> getMaps() {
+        return this.activeMaps;
+
+    }
+
+    public void shuffleMaps() {
+        Collections.shuffle(this.activeMaps);
+    }
+
+    public void unshuffleMaps() {
+        this.activeMaps = new ArrayList<>(this.baseMaps);
+    }
 }
