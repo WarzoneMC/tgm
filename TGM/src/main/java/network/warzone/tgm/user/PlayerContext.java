629d0fd6324a67b2f245e5d340a59a40bf052472
PundaChen <42002575+PundaChen@users.noreply.github.com>
2021-01-25 20:57:01 +0100

    revert colours
    I dont speak code so idk if this will work

--- TGM/src/main/java/network/warzone/tgm/user/PlayerContext.java
+++ TGM/src/main/java/network/warzone/tgm/user/PlayerContext.java
@@ -1,103 +1,89 @@
 package network.warzone.tgm.user;
-
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import net.md_5.bungee.api.ChatColor;
 import network.warzone.tgm.TGM;
 import network.warzone.tgm.util.Ranks;
 import network.warzone.warzoneapi.models.Rank;
 import network.warzone.warzoneapi.models.UserProfile;
-import net.md_5.bungee.api.ChatColor;
-
 import org.bukkit.entity.Player;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
-
 /**
  * Created by luke on 4/27/17.
  */
 public class PlayerContext {
     @Getter private Player player;
     private UserProfile userProfile;
-    private static final List<PlayerLevel> levels = new ArrayList<>();
-
-    static {
-        levels.add(new PlayerLevel((lvl) -> lvl < 10, ChatColor.of("#BBBAD3")));
-        levels.add(new PlayerLevel((lvl) -> lvl < 20, ChatColor.of("#AAC1FF")));
-        levels.add(new PlayerLevel((lvl) -> lvl < 30, ChatColor.of("#B1AAFF")));
-        levels.add(new PlayerLevel((lvl) -> lvl < 40, ChatColor.of("#AAE1FF")));
-        levels.add(new PlayerLevel((lvl) -> lvl < 50, ChatColor.of("#AAFFFF")));
-        levels.add(new PlayerLevel((lvl) -> lvl < 60, ChatColor.of("#96FFBC")));
-        levels.add(new PlayerLevel((lvl) -> lvl < 70, ChatColor.of("#ABFFA5")));
-        levels.add(new PlayerLevel((lvl) -> lvl < 80, ChatColor.of("#D9FFAA")));
-        levels.add(new PlayerLevel((lvl) -> lvl < 90, ChatColor.of("#FFFFAA")));
-        levels.add(new PlayerLevel((lvl) -> lvl < 100, ChatColor.of("#FFD9AA")));
-        levels.add(new PlayerLevel((lvl) -> lvl < 120, ChatColor.of("#FFAAAA")));
-        levels.add(new PlayerLevel((lvl) -> lvl < 140, ChatColor.of("#FFAAC9")));
-        levels.add(new PlayerLevel((lvl) -> lvl < 160, ChatColor.of("#FF96DA")));
-        levels.add(new PlayerLevel((lvl) -> lvl < 180, ChatColor.of("#FF66FF")));
-        levels.add(new PlayerLevel((lvl) -> lvl < 200, ChatColor.of("#E266FF")));
-        levels.add(new PlayerLevel((lvl) -> lvl < 220, ChatColor.of("#C966FF")));
-        // fallback
-        levels.add(new PlayerLevel((lvl) -> true, ChatColor.of("#9E66FF")));
-    }
-
     public PlayerContext(Player player, UserProfile userProfile) {
         this.player = player;
         this.userProfile = userProfile;
     }
-
     public UserProfile getUserProfile() {
         return getUserProfile(false);
     }
-
     public UserProfile getUserProfile(boolean original) {
-        if (isNicked() && !original) {
-            return TGM.get().getNickManager().getNick(this).get().getProfile();
+        if (hasNickedStats() && isNicked() && !original) {
+            return TGM.get().getNickManager().getStats().get(player.getUniqueId());
         } else {
             return userProfile;
         }
     }
-
     public String getDisplayName() {
-        if (isNicked()) {
-            return TGM.get().getNickManager().getNick(this).get().getName();
-        }
-        return player.getName();
+        return TGM.get().getNickManager().getNickNames().getOrDefault(player.getUniqueId(), player.getName());
     }
-
     public String getOriginalName() {
-        if (isNicked()) {
-            return TGM.get().getNickManager().getNick(this).get().getOriginalName();
-        }
-        return player.getName();
+        return TGM.get().getNickManager().getOriginalNames().getOrDefault(player.getUniqueId(), player.getName());
     }
-
+    public boolean hasNickedStats() {
+        return TGM.get().getNickManager().getStats().containsKey(player.getUniqueId());
+    }
     public boolean isNicked() {
-        return TGM.get().getNickManager().isNicked(this);
+        return TGM.get().getNickManager().getNickNames().containsKey(player.getUniqueId());
     }
-
     public String getLevelString() {
         return getLevelString(false);
     }
-
     public String getLevelString(boolean original) {
         int level = getUserProfile(original).getLevel();
-        for (PlayerLevel levelEntry : levels) {
-            if (levelEntry.check.test(level)) {
-                return "" + levelEntry.levelColor + "[" + level + "]"; 
-            }
-        }
 
-        // will not be reached due to fallback entry
-        return null;
+
+        if (level < 10) {
+            return ChatColor.GRAY + "[" + level + "]";
+        }
+        else if (level < 20) {
+            return ChatColor.DARK_AQUA + "[" + level + "]";
+        }
+        else if (level < 30) {
+            return ChatColor.BLUE + "[" + level + "]";
+        }
+        else if (level < 40) {
+            return ChatColor.LIGHT_PURPLE + "[" + level + "]";
+        }
+        else if (level < 60) {
+            return ChatColor.DARK_PURPLE + "[" + level + "]";
+        }
+        else if (level < 80) {
+            return ChatColor.DARK_RED + "[" + level + "]";
+        }
+        else if (level < 100) {
+            return ChatColor.RED + "[" + level + "]";
+        }
+        else if (level < 120) {
+            return ChatColor.GOLD + "[" + level + "]";
+        }
+        else if (level < 140) {
+            return ChatColor.YELLOW + "[" + level + "]";
+        }
+        else if (level < 160) {
+            return ChatColor.GREEN + "[" + level + "]";
+        } else {
+            return ChatColor.DARK_GREEN + "[" + level + "]";
+        }
     }
 
     public void updateRank(Rank r) {
         updateRank(r, false);
     }
-
     public void updateRank(Rank r, boolean forceUpdate) {
         List<String> oldPermissions = new ArrayList<>();
         boolean update = false;
@@ -113,12 +99,5 @@ public class PlayerContext {
             Ranks.removePermissions(player, oldPermissions);
             getUserProfile().getRanksLoaded().forEach(rank -> Ranks.addPermissions(player, rank.getPermissions()));
         }
-
-    }
-
-    @AllArgsConstructor @Getter
-    private static class PlayerLevel {
-        private final Predicate<Integer> check;
-        private final ChatColor levelColor;
     }
 }
